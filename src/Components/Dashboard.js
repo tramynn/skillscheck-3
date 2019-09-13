e838a350653c8e26195f11795e7e82d8735853b8
Tramy Nguyen <tramynn2@gmail.com>
2019-09-13 11:49:30 -0500

    set up dashboard

---
+++ src/Components/Dashboard.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Dashboard extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="Dashboard">
+        <header className=""></header>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
